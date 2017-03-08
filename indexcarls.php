71a5c370b32850a63de21a5eb4329c5039238bef
Heribertolh123 <heriberto.lopez.hdez@gmail.com>
2017-03-08 14:11:57 -0600

    Add files via upload

---
+++ indexcarls.php
@@ -0,0 +1,19 @@
+<?php
+   $db_host = "am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+   $db_name = "lalz76htoqmmiv2r";
+   $db_user = "j3suevtbx1kkob0a";
+   $db_password = "ejonmlc7rm6ha11x";
+    
+  $connection = mysqli_connect('am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'j3suevtbx1kkob0a', 'ejonmlc7rm6ha11x');
+    
+    mysqli_select_db($connection, $db_name) or die("Error al seleccionar la base de datos:".mysqli_error());
+    @mysqli_query("SET NAMES 'utf8'");
+
+   $sql_query = "SELECT * FROM contactos;";
+   $result = mysqli_query($sql_query);
+   $rows = array();
+while($r = mysqli_fetch_assoc($result)) {
+  $rows[] = $r;
+}
+print json_encode($rows);
+?>
